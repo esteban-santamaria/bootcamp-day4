ab9c67066df397ba8ce2e7c057dac9ff6e53d7fd
Esteban Santamaria <esteban.santamaria@yuxpacific.com>
2018-02-15 07:33:26 -0500

    tour of heroes

---
+++ src/app/hero.service.ts
@@ -0,0 +1,16 @@
+import { MessagesService } from './messages.service';
+import { Injectable } from '@angular/core';
+import { Hero } from './hero';
+import { HEROES } from './mock-heroes';
+import { Observable } from 'rxjs/Observable';
+import { of } from 'rxjs/observable/of';
+
+@Injectable()
+export class HeroService {
+	constructor(private messagesService: MessagesService) {}
+
+	getHeroes(): Observable<Hero[]> {
+		this.messagesService.add('HeroService: fetched heroes');
+		return of(HEROES);
+	}
+}
